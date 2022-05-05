fa4f17f2219f1dd04b1a03101868a23fdb56c530
liweidong <357879926@qq.com>
2022-05-05 15:51:46 +0800

    dev

--- hb/include/hb/crypto/hex.h
+++ hb/include/hb/crypto/hex.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <string>
 
-namespace hb {
-    namespace crypto {
+namespace hb::crypto {
 
-        std::string hex(const std::string &str);
-        std::string hex_to_str(const std::string &hex_str);
-    }  // namespace crypto
+    std::string hex(const std::string &str);
+    std::string hex_to_str(const std::string &hex_str);
+    int64_t hex_to_dec(std::string hex_str);
+    std::string dec_to_hex(int64_t decn);
 }  // namespace hb
--- hb/src/crypto/hex.cpp
+++ hb/src/crypto/hex.cpp
@@ -1,23 +1,51 @@
 #include <hb/crypto/cryptopp.h>
 #include <hb/crypto/hex.h>
+#include <string>
+#include <boost/algorithm/string.hpp>
 
-namespace hb {
-    namespace crypto {
+namespace hb::crypto {
 
-        using namespace CryptoPP;
+    using namespace CryptoPP;
 
-        std::string hex(const std::string &str) {
-            std::string encoded;
-            StringSource ss(str, true, new HexEncoder(new StringSink(encoded)));
-            return encoded;
+    std::string hex(const std::string &str) {
+        std::string encoded;
+        StringSource ss(str, true, new HexEncoder(new StringSink(encoded)));
+        return encoded;
+    }
+
+    std::string hex_to_str(const std::string &hex_str) {
+        std::string decoded;
+        StringSource ssk(hex_str, true /*pump all*/,
+                            new HexDecoder(new StringSink(decoded))  // HexDecoder
+        );
+        return decoded;
+    }
+
+    int64_t hex_to_dec(std::string hex_str)
+    {
+        if (boost::istarts_with(hex_str, "0x")) { // 不区分大小写
+            hex_str = std::string(hex_str.begin()+2, hex_str.end());
         }
+        int64_t n;
+        std::istringstream(hex_str) >> std::hex >> n;
+        return n;
+    }
 
-        std::string hex_to_str(const std::string &hex_str) {
-            std::string decoded;
-            StringSource ssk(hex_str, true /*pump all*/,
-                             new HexDecoder(new StringSink(decoded))  // HexDecoder
-            );
-            return decoded;
-        }
-    }  // namespace crypto
+    //十进制数字转十六进字符串
+    std::string dec_to_hex(int64_t decn)
+    {
+        std::stringstream ss;
+        ss<<std::hex<<std::showbase<<std::nouppercase<<decn;
+        std::string hex_str=ss.str();
+        return hex_str;
+    }
+
+    int oct_to_dec(std::string octstr)
+    {
+        int decn;
+        std::stringstream ss;
+        ss.str(octstr);
+        ss<<std::oct<<std::showbase<<std::nouppercase<<decn;
+        return decn;
+    }
 }  // namespace hb
