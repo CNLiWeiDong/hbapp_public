1266faa77405a820b315fa5792a0afca0b4200b7
liweidong <357879926@qq.com>
2022-05-22 23:44:29 +0800

    dev

--- plugins/send_mail_plugin/send_mail_plugin_impl.cpp
+++ plugins/send_mail_plugin/send_mail_plugin_impl.cpp
@@ -12,22 +12,32 @@ namespace hb {
         send_mail_plugin_impl::~send_mail_plugin_impl() {}
         void send_mail_plugin_impl::send_mail(const string &to, const string &subject,
                                               const string &content) {
-            auto &thread_plugin = app().get_plugin<thread_pool_plugin>();
-            auto api = thread_plugin.get_api();
-            auto self = shared_from_this();
-            std::string task_id = "send_mail_" + to_string(send_mail_task_id++);
-            log_info("send mail to: %s thread task is: %s", to.c_str(), task_id.c_str());
-            api->post(task_id, [=]() {
-                session sess;
-                sess.host(self->host());
-                sess.port(self->port());
-                sess.user(self->user());
-                sess.pass(self->pass());
-                sess.to(to);
-                sess.subject(subject);
-                sess.content(content);
-                sess.mail();
-            });
+
+            session sess;
+            sess.host(host());
+            sess.port(port());
+            sess.user(user());
+            sess.pass(pass());
+            sess.to(to);
+            sess.subject(subject);
+            sess.content(content);
+            sess.mail();
+            // auto &thread_plugin = app().get_plugin<thread_pool_plugin>();
+            // auto api = thread_plugin.get_api();
+            // auto self = shared_from_this();
+            // std::string task_id = "send_mail_" + to_string(send_mail_task_id++);
+            // log_info("send mail to: %s thread task is: %s", to.c_str(), task_id.c_str());
+            // api->post(task_id, [=]() {
+            //     session sess;
+            //     sess.host(self->host());
+            //     sess.port(self->port());
+            //     sess.user(self->user());
+            //     sess.pass(self->pass());
+            //     sess.to(to);
+            //     sess.subject(subject);
+            //     sess.content(content);
+            //     sess.mail();
+            // });
         }
     }  // namespace plugin
 }  // namespace hb
